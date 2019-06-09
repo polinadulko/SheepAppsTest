373c5b81fb18268e804e6783e233444d811c9027
Polina <polinmailbox@gmail.com>
2019-06-09 12:45:09 +0300

    Initial Commit

---
+++ SheepAppsTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SheepAppsTest
+//
+//  Created by Polina Dulko on 6/9/19.
+//  Copyright © 2019 Polina Dulko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
